b2dce840504b4cd7d1e7b7cf0fbdc481c817cffe
devibukun <mhizdivylashey@gmail.com>
2024-09-04 10:03:05 +0100

    .

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb:1,
+        question: "What is the purpose of  the <header> element in HTML?",
+        answer: "B. To define a header section.",
+        options:[
+               "A. To define a footer section.",
+               "B. To define a header section.",
+               "C. To define a navigation menu.",
+               "D. To define a main content section."
+            ]
+    },
+    {
+        numb:2,
+        question: "What is the difference between MARGIN and PADDING in CSS?",
+        answer: "A. Margin is used for spacing between elements, while padding is used for spacing within element.",
+        options:[
+                "A. Margin is used for spacing between elements, while padding is used for spacing within element.",
+                "B. Margin is used for spacing within an element, while padding is used for spacing between elements.",
+                "C. Margin is used for borders, while padding is used for backgrounds.",
+                "D. Margin is used for backgrounds, while padding is used for borders.",   
+             ]
+    },
+    {
+        numb:3,
+        question: "What is the purpose of the this keyword in JavaScript?",
+        answer: "B. To refer to the current object.",
+        options:[
+            "A. To refer to the global object.",
+            "B. To refer to the current object.",
+            "C. To refer to the parent object.",
+            "D. To refer to the child element.",
+        ]
+    },
+    {
+        numb:4,
+        question: "What is the purpose of the data- attribute in HTML?",
+        answer: "A. To store custom data attributes.",
+        options:[
+               "A. To store custom data attributes.",
+               "B. To store CSS Styles.",
+               "C. To store JavaScript code.",
+               "D. To store HTML content.",
+        ]
+    },
+    {
+        numb:5,
+        question: "The difference between == and === in JavaScript?",
+        answer: "A. == checks for value equality, while === checks for type and equality, while === checks for type and value equality.",
+        options:[
+               "A. == checks for value equality, while === checks for type and equality, while === checks for type and value equality.",
+                "B. == checks for type and value equality while === checks for value equality.",
+                "C. == checks for type equality, while === checks for value equality.",
+                "D. == checks for value equality, while === checks for type equality.",
+            ]
+    }
+    ];
--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const menuButton = document.getElementById('menuBtn');
 const menu = document.getElementById('menu');
 const sideBar = document.getElementById('side-navbar');
 const about = document.getElementById("about");
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const container = document.querySelector('.container');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgainBtn = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
 
 // To show pop-up information using the start button/add background blur
 startBtn.onclick = () => {
@@ -16,6 +23,7 @@ startBtn.onclick = () => {
      about.style.display = "none";
      document.body.style.overflowY = "hidden";
     
+    
 }
 
 // To close the pop upinformation using the exit quiz button/remove background blur
@@ -24,7 +32,7 @@ exitBtn.onclick = () => {
     main.classList.remove('active'); 
     navigation.classList.remove('active');
     about.style.display = "block";
-     document.body.style.overflowY = "auto";
+    document.body.style.overflowY = "auto";
 }
 
 // Side MenuBar Functionality
@@ -35,3 +43,161 @@ navigation.onclick = function(){
         navigation.style.right = "-200px";
     }
 }
+
+// Continue Button Functionality
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active'); 
+    quizSection.style.display = "block";
+   
+    setTimeout(() =>{
+       quizSection.style.left = '0';
+    },50);
+    container.style.display = "flex";
+    quizBox.classList.add('active'); 
+    navigation.classList.remove('active'); 
+    
+    showQuestions(0);
+    questionCounter(1);
+    headerScore();
+}
+
+tryAgainBtn.onclick = () => {
+    quizBox.classList.add('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
+    headerScore();
+}
+
+goHomeBtn.onclick = () => {
+    document.querySelector('.container').classList.remove('active');
+    document.querySelector('.container').style.display= 'none';
+    document.querySelector('.home').style.display = 'flex';
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+    document.body.style.overflowY = "auto";
+    about.style.display = "block";
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
+    headerScore();
+}
+
+let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+nextBtn.onclick = () => {
+    if(questionCount < questions.length -1){
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+
+        nextBtn.classList.remove('active');
+
+    }else{
+        showResultBox();
+    }
+  
+}
+
+const optionList = document.querySelector('.option-list');
+
+// GETTING QUESTIONS AND OPTIONS FROM ARRAY
+
+function showQuestions(index){
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for(let  i = 0; i< option.length; i++){
+         option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer === correctAnswer){
+        answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
+    }else{
+        answer.classList.add('incorrect');
+
+        // if user answer is wrong, auto select the correct one
+        for (let i = 0; i < allOptions; i++){
+           if(optionList.children[i].textContent == correctAnswer){
+            optionList.children[i].setAttribute('class', 'option correct');
+           }
+        }
+    }
+
+
+    // if user has selected, disabled all options
+for (let i = 0; i < allOptions; i++){
+    optionList.children[i].classList.add('disabled');
+}
+nextBtn.classList.add('active');
+}
+
+
+
+function questionCounter(index){
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent =  `${index} of ${questions.length} Questions`;
+}
+
+function headerScore(){
+    const headerScoreText = document.querySelector('.header-score');
+    headerScoreText.textContent = `Score: ${userScore} / ${questions.length} `;
+}
+
+
+function showResultBox(){
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
+
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+
+    let progress = setInterval(() =>{
+        progressStartValue++;
+        circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(225,255,255,.1) 0deg)`;
+      progressValue.textContent = `${progressStartValue}%`;
+        if(progressStartValue == progressEndValue){
+            clearInterval(progress);
+        }
+
+    },speed);
+} 
